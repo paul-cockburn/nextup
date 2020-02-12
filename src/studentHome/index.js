a88be6fb0fba1adb016c59ed6c3d8f993d08286a
paulcockburn <paul.s.cockburn@gmail.com>
2020-02-12 15:33:23 +0000

    display default request state in card

--- src/studentHome/index.js
+++ src/studentHome/index.js
@@ -1,13 +1,20 @@
 import * as firebase from "firebase";
 import React from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 
 class StudentHome extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+          requestId: "0",
+          requestDescription: "Request description",
+          requestTime: "00:00",
+          requestClass: "F20DL",
+          requestLocation: "I'm sitting in EM2.45 in the back left corner on computer 15.",
+          requestPriority: "Medium"
+        };
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -41,7 +48,30 @@ class StudentHome extends React.Component {
                 </Button>
             </Link>
             <h2>Your Requests</h2>
-            {/* <p>{this.state}</p> */}
+
+            <Card>
+              <Card.Header as="h5">Request ID: {this.state.requestId}</Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  <strong>Time: </strong>{this.state.requestTime}
+                </Card.Text>
+                <Card.Text>
+                  <strong>Description: </strong>{this.state.requestDescription}
+                </Card.Text>
+                <Card.Text>
+                <strong>Class: </strong>{this.state.requestClass}
+                </Card.Text>
+                <Card.Text>
+                <strong>Location: </strong>{this.state.requestLocation}
+                </Card.Text>
+                <Card.Text>
+                <strong>Priority: </strong>{this.state.requestPriority}
+                </Card.Text>
+                <Button variant="primary">Edit</Button>
+                <Button variant="primary">Delete</Button>
+              </Card.Body>
+            </Card>
+
             <Button onClick={this.handleSubmit}>Refresh</Button>
         </div>
       );
