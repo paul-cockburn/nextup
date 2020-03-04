7e073f88b6f119efc56ee8f076994dd2902f663d
paul-cockburn <paul.s.cockburn@gmail.com>
2020-03-04 16:40:59 +0000

    added statistics with accordion

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import StudentHome from './studentHome';
 import RequestHelp from './requestHelp';
 import HelperHome from './helperHome';
 import Overview from './overview';
+import Statistics from './statistics';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import { BrowserRouter, Route } from "react-router-dom";
@@ -32,6 +33,7 @@ function App() {
               <Route path="/request-help" exact component={RequestHelp}  />
               <Route path="/helper-home" exact component={HelperHome}  />
               <Route path="/overview" exact component={Overview}  />
+              <Route path="/statistics" exact component={Statistics}  />
             </Col>
             <Col></Col>
           </Row>
