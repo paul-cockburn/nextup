734b8130f5a90a16ed3c39fdc51c3dcae7aa7229
paul-cockburn <paul.s.cockburn@gmail.com>
2020-03-15 19:21:43 +0000

    sorting on helper home

--- src/helperHome/index.js
+++ src/helperHome/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Dropdown, Badge, Accordion } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import RequestCard from "../components/RequestCard"
-
+import * as moment from "moment";
 
 class HelperHome extends React.Component {
     constructor(props) {
@@ -15,12 +15,14 @@ class HelperHome extends React.Component {
         requestClass: "F20DL",
         requestLocation: "I'm sitting in EM2.45 in the back left corner on computer 15.",
         requestPriority: "Medium",
-        waitingTotal: 0
+        waitingTotal: 0,
+        sortBy: "recommended"
       };
   
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
       this.getRequests = this.getRequests.bind(this);
+      this.recommended = this.recommended.bind(this);
       this.oldestFirst = this.oldestFirst.bind(this);
       this.newestFirst = this.newestFirst.bind(this);
       this.highPriFirst = this.highPriFirst.bind(this);
@@ -76,6 +78,12 @@ class HelperHome extends React.Component {
     handleSubmit(event) {
     }
 
+    recommended(){
+      this.setState({
+        sortBy: "recommended"
+      })
+    }
+
     oldestFirst(){
       this.setState({
         sortBy: "oldestFirst"
@@ -106,7 +114,7 @@ class HelperHome extends React.Component {
             <h1>Home</h1>
             <h2>Your Active Requests</h2>
 
-            <ReturnCards requests={this.state.inProgReqs} currentUser={this.state.currentUser}/>
+            <ReturnCards requests={this.state.inProgReqs} currentUser={this.state.currentUser} sortBy={this.state.sortBy}/>
 
             <h2>Requests Waiting <Badge variant="info">{this.state.waitingTotal}</Badge></h2>
 
@@ -116,6 +124,7 @@ class HelperHome extends React.Component {
               </Dropdown.Toggle>
 
               <Dropdown.Menu>
+                <Dropdown.Item onClick={this.recommended}>Recommended</Dropdown.Item>
                 <Dropdown.Item onClick={this.oldestFirst}>Oldest first</Dropdown.Item>
                 <Dropdown.Item onClick={this.newestFirst}>Newest first</Dropdown.Item>
                 <Dropdown.Item onClick={this.highPriFirst}>Highest priority first</Dropdown.Item>
@@ -123,36 +132,65 @@ class HelperHome extends React.Component {
               </Dropdown.Menu>
             </Dropdown>
 
-            <ReturnCards requests={this.state.waitingReqs} currentUser={this.state.currentUser}/>
+            <ReturnCards requests={this.state.waitingReqs} currentUser={this.state.currentUser} sortBy={this.state.sortBy}/>
 
         </div>
       );
     }
   }
 
-  function ReturnCards({ requests, currentUser }) {
+  function ReturnCards({ requests, currentUser, sortBy }) {
+    console.log(!requests)
     if (!requests) {
-      return <p>No requests</p>;
+      return (<p>No requests</p>);
     }
+
+    var reqsArray = Object.values(requests);
+    reqsArray.sort(function(a, b){
+      moment.defaultFormat = "DD.MM.YYYY HH:mm";
+      var aFormatted = moment(a.requestTime, moment.defaultFormat).toDate()
+      var bFormatted = moment(b.requestTime, moment.defaultFormat).toDate()
+      var aDate = new Date(aFormatted)
+      var bDate = new Date(bFormatted)
+      if(sortBy === "oldestFirst"){
+        return (
+          (aDate.getTime())-(bDate.getTime())
+        );
+      }else if(sortBy === "newestFirst"){
+        return (
+          (bDate.getTime())-(aDate.getTime())
+        );      
+      }else if(sortBy === "recommended"){
+        return (
+          (a.hVal-b.hVal)
+        );      
+      }
+    })
+    const arrayToObject = (array) =>
+      array.reduce((obj, item) => {
+        obj[item.requestId] = item
+        return obj
+      }, {})
+    const requestsSorted = arrayToObject(reqsArray)
 
     return (
       <div>
         <Accordion>
-        {Object.keys(requests).map(requestKey => (
+        {Object.keys(requestsSorted).map(requestKey => (
             <RequestCard key={requestKey}
                 requestId = {requestKey}
-                requestUser = {requests[requestKey].requestUser}
-                requestDescription = {requests[requestKey].requestDescription}
-                requestTime = {requests[requestKey].requestTime}
-                requestClass = {requests[requestKey].requestClass}
-                requestLocation = {requests[requestKey].requestLocation}
-                requestPriority = {requests[requestKey].requestPriority}
-                requestStatus = {requests[requestKey].requestStatus}
-                requestHelper = {requests[requestKey].requestHelper}
+                requestUser = {requestsSorted[requestKey].requestUser}
+                requestDescription = {requestsSorted[requestKey].requestDescription}
+                requestTime = {requestsSorted[requestKey].requestTime}
+                requestClass = {requestsSorted[requestKey].requestClass}
+                requestLocation = {requestsSorted[requestKey].requestLocation}
+                requestPriority = {requestsSorted[requestKey].requestPriority}
+                requestStatus = {requestsSorted[requestKey].requestStatus}
+                requestHelper = {requestsSorted[requestKey].requestHelper}
                 helperCard = {true}
                 studentCard = {false}
                 requestHelper = {currentUser}
-                cardColor = {requests[requestKey].cardColor}
+                cardColor = {requestsSorted[requestKey].cardColor}
             />
         ))}
         </Accordion>
